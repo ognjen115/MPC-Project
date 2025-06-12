67209637e325bd4d01d6fab983a95be5c15fde04
Ognjen Lukic <e12029403@student.tuwien.ac.at>
2025-06-12 18:01:49 +0200

    task12

--- templates/lqr_maxPI.m
+++ templates/lqr_maxPI.m
@@ -10,34 +10,29 @@
 
 function [H_LQR, h_LQR] = lqr_maxPI(Q, R, params)
     % Extract system matrices from params
-    A = params.model.A; % [nx x nx]
-    B = params.model.B; % [nx x nu]
-    H_x = params.constraints.StateMatrix; % [n_cx x nx]
-    h_x = params.constraints.StateRHS; % [n_cx x 1]
-    H_u = params.constraints.InputMatrix; % [n_cu x nu]
-    h_u = params.constraints.InputRHS; % [n_cu x 1]
-
-    % Debug: Print dimensions to verify
-    disp(['Size of A: ', mat2str(size(A))]);
-    disp(['Size of B: ', mat2str(size(B))]);
+    A = params.model.A; 
+    B = params.model.B; 
+    H_x = params.constraints.StateMatrix; 
+    h_x = params.constraints.StateRHS; 
+    H_u = params.constraints.InputMatrix; 
+    h_u = params.constraints.InputRHS; 
 
     % Compute LQR feedback gain K
-    [K, ~, ~] = dlqr(A, B, Q, R); % K is [nu x nx]
-    disp(['Size of K: ', mat2str(size(K))]);
+    [K, ~, ~] = dlqr(A, B, Q, R); 
 
-    % Define F_inf as per document: u = F_inf * x
-    F_inf = -K; % [nu x nx]
+    % Define F_inf: u = F_inf * x
+    F_inf = -K; 
 
     % Define closed-loop system matrix
-    A_cl = A + B * F_inf; % [nx x nx], equivalent to A - B * K
+    A_cl = A + B * F_inf; 
 
     % Define constraint polyhedron
-    H_input = H_u * F_inf; % [n_cu x nx]
-    h_input = h_u; % [n_cu x 1]
+    H_input = H_u * F_inf; 
+    h_input = h_u; 
 
     % Combine state and input constraints
-    H_total = [H_x; H_input]; % [ (n_cx + n_cu) x nx ]
-    h_total = [h_x; h_input]; % [ (n_cx + n_cu) x 1 ]
+    H_total = [H_x; H_input]; 
+    h_total = [h_x; h_input]; 
 
     % Create polyhedron for constraints
     P = Polyhedron('A', H_total, 'b', h_total);
@@ -51,27 +46,22 @@ function [H_LQR, h_LQR] = lqr_maxPI(Q, R, params)
 end
 
 function X = computeMaxPosInvariantSet(P, A_cl)
-    % Iterative computation of the maximum positively invariant set
-    max_iter = 100; % Maximum iterations to prevent infinite loop
-    tol = 1e-6; % Tolerance for convergence
+    max_iter = 100; % Prevent infinite loop
 
     X = P; % Start with the constraint set
     for i = 1:max_iter
-        % Compute the pre-image manually: X_prev = { x | A_cl * x \in X }
-        H = X.A; % Constraints of current set X
-        h = X.b;
-        % Pre-image constraints: H * (A_cl * x) <= h => (H * A_cl) * x <= h
-        H_prev = H * A_cl; % New constraint matrix
-        h_prev = h; % Same RHS
+        % Compute the pre-image of X
+        H_prev = X.A * A_cl;
+        h_prev = X.b;
 
         % Define the pre-image polyhedron
         X_prev = Polyhedron('A', H_prev, 'b', h_prev);
 
         % Intersect with the original constraint set
-        X_new = X_prev & P; % Intersection using MPT3
+        X_new = X_prev & P;
 
         % Check for convergence
-        if X_new == X % MPT3 equality check for polyhedra
+        if X_new == X
             break;
         end
 
@@ -82,6 +72,7 @@ function X = computeMaxPosInvariantSet(P, A_cl)
         warning('MaxPosInvariantSet: Did not converge within max iterations');
     end
 end
+
 
 
 
